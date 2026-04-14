eed0a44ac6fe9a47cb849f429948207e48cae149
armanmulla-2007 <armanriyajmulla07@gmail.com>
2026-04-14 21:01:25 +0530

    Day 3: Refactored code using functions, added input validation and file handling

--- dailytracker.c
+++ dailytracker.c
@@ -1,35 +1,85 @@
 #include<stdio.h>
-int main(){
-  int task[9];
-  char *name[]={"Fajr","Zuhr","Asr","Maghrib","Isha","coding","github","Quran","study"};
+#define SIZE 9
 
-  int score=0;
-
-  printf("\n ~~~~~~DAILY TRACKEER~~~~~~");
-  printf("\n Marking Methods : 1=Done, 0=Not Done");
-
-  for(int i=0;i<9;i++){
-     printf("\n%s:",name[i]);
-     scanf("%d",&task[i]);
-     score+=task[i];
-  }
-
-  printf("\n ~~~~~~DAILY REPORT~~~~~~");
-  printf("\n Namaz:%s",(task[0] && task[1] && task[2] && task[3] && task[4]) ? "Complete" : "Incomplete");
-
-  for(int i=5;i<9;i++){
-    printf("\n %s:%s",name[i],task[i] ? "Done" : "Not Done");
-  }
-
-  printf("\n total score:%d",score);
-
-  printf("\n ~~~~~~DAILY PROGRESS~~~~~~");
-  printf("\n %s",(score>=8) ? "Excellent day":
-                 (score<=8 && score>5) ? "Good, But need Improvement":
-                                         "Focus More Tomorrow");
-  return 0;
+void takeInput(int task[], char *name[]){
+   for(int i=0;i<SIZE;i++){
+      do{
+         printf("\n%s(0 or 1):",name[i]);
+         scanf("%d",&task[i]);
+      }while(task[i]!=0 && task[i]!=1);
+   }
 }
 
-   
-  
+int calculateScore(int task[]){
+   int score = 0;
+    for(int i=0;i<SIZE;i++){
+       score +=task[i];
+    }
+   return score;
+}
 
+int checkNamaz(int task[]){
+    for(int i=0;i<5;i++){
+        if(task[i]==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void displayReport(int task[], char *name[], int score){
+     printf("\n\n~~~~~~ DAILY REPORT ~~~~~~");
+
+      printf("\nNamaz: %s", checkNamaz(task) ? "Complete" : "Incomplete");
+
+    for(int i = 5; i < SIZE; i++) {
+        printf("\n%s: %s", name[i], task[i] ? "Done" : "Not Done");
+    }
+
+    printf("\nTotal Score: %d", score);
+}
+
+void displayProgress(int score) {
+    printf("\n\n~~~~~~ DAILY PROGRESS ~~~~~~");
+
+    if(score >= 8) {
+        printf("\nExcellent Day 🔥");
+    }
+    else if(score > 5) {
+        printf("\nGood, But Need Improvement 👍");
+    }
+    else {
+        printf("\nFocus More Tomorrow 💪");
+    }
+}
+
+void saveToFile(int score) {
+    FILE *fp = fopen("report.txt", "a");
+
+    if(fp == NULL) {
+        printf("\nError saving file!");
+        return;
+    }
+
+    fprintf(fp, "Score: %d\n", score);
+    fclose(fp);
+  }
+
+  int main() {
+    int task[SIZE];
+    char *name[] = {"Fajr","Zuhr","Asr","Maghrib","Isha","Coding","GitHub","Quran","Study"};
+
+    printf("\n~~~~~~ DAILY TRACKER ~~~~~~");
+    printf("\nMarking Method: 1 = Done, 0 = Not Done\n");
+
+    takeInput(task, name);
+
+    int score = calculateScore(task);
+
+    displayReport(task, name, score);
+    displayProgress(score);
+
+    saveToFile(score);
+
+    return 0;
+}
